0c1aac04bd4c9e7541183a56a113fb7b0f815ef8
Aditya Pagare <157268686+LogicRoamer-ai@users.noreply.github.com>
2024-01-21 12:21:30 +0530

    Add files via upload

---
+++ username_password.py
@@ -0,0 +1,20 @@
+# abc = "ESaalaCupnamDe"
+#print("ESaala",abc[6:9],abc[9:14]
+username = ['Aditya','gaurav','MacchiMarket']
+password = ['don','rigid','Eww']
+security = input(" enter username:") 
+if security == (username[0] or username[1] or username[2] ):
+    pwd=input("Enter Password :")
+    if (security == username[0] and  pwd == password[0]) or (security == username[1] and  pwd == password[1]) or (security == username[2] and  pwd == password[2]):
+        print(" Welcome")
+    else :
+        print("barobar dal re baba")
+else :
+    print("Not Valid username")
+# print(pwd) 
+# abc =input("username")
+# if  abc == username[0]:
+#     print("Enter Password")
+# else :
+#     print("Not Valid")
+# print(username)
